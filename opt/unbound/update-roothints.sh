e6d83e806d389ab826c2a087b3c74dfde9046384
Oleksii Usatov <62352097+ousatov-ua@users.noreply.github.com>
2024-01-14 21:20:29 +0200

    Update update-roothints.sh

--- opt/unbound/update-roothints.sh
+++ opt/unbound/update-roothints.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-wget ftp://FTP.INTERNIC.NET/domain/named.cache -O /usr/local/etc/unbound/root.hints
+# wget ftp://FTP.INTERNIC.NET/domain/named.cache -O /usr/local/etc/unbound/root.hints
+# IANA as source for root hints
+wget https://www.iana.org/domains/root/files -O /usr/local/etc/unbound/root.hints
+# Generate root key
 unbound-anchor -a "/usr/local/etc/unbound/root.key"
+# Reload config
 unbound-control reload_keep_cache
