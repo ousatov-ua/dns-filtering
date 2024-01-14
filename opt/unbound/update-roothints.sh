a9ab84f65d25d39d8880af5b91d480bfd3eef7db
Oleksii Usatov <62352097+ousatov-ua@users.noreply.github.com>
2024-01-14 21:15:15 +0200

    Update update-roothints.sh

--- opt/unbound/update-roothints.sh
+++ opt/unbound/update-roothints.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget ftp://FTP.INTERNIC.NET/domain/named.cache -O /usr/local/etc/unbound/root.hints
+unbound-anchor -a "/usr/local/etc/unbound/root.key"
 unbound-control reload_keep_cache
