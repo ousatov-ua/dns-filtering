37ba272865f0e069bd72afbf4184f757b6b5927a
Oleksii Usatov <62352097+ousatov-ua@users.noreply.github.com>
2024-01-31 18:43:30 +0200

    Create update-blocklists.sh

---
+++ opt/bind9/update-blocklists.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -o "/etc/bind/blocklists/hagezy-multi-normal.rpz" "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/rpz/multi.txt"
+
+curl -o "/etc/bind/blocklists/hagezy-gambling.rpz" "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/rpz/gambling.txt"
+
+curl -o "/etc/bind/blocklists/oisd-nsfw.rpz" "https://nsfw.oisd.nl/rpz"
+
+curl -o "/etc/bind/blocklists/hagezy-anti-privacy.rpz" "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/rpz/anti.piracy.txt"
+
+curl -o "/etc/bind/blocklists/hagezy-no-safe-search.rpz" "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/rpz/nosafesearch.txt"
+
+curl -o "/etc/bind/blocklists/hagezy-threat.rpz" "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/rpz/tif.txt"
+
+curl -o "/etc/bind/blocklists/olus-blocklist.rpz" "https://raw.githubusercontent.com/ousatov-ua/dns-filtering/main/blocklist/olus-blocklist.rpz"
+
+rndc reload
