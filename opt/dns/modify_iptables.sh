7039e0a4b086ccaa296f07cf609368dd57e3da80
Oleksii Usatov <62352097+ousatov-ua@users.noreply.github.com>
2024-02-11 19:32:22 +0200

    Update modify_iptables.sh

--- opt/dns/modify_iptables.sh
+++ opt/dns/modify_iptables.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+#Specifically, many Linux distributions run with a connection tracking firewall configured. For high load operation (thousands of queries/second), 
+#It is advised to either turn off iptables completely, or use the NOTRACK feature to make sure client DNS traffic bypasses the connection tracking.
 iptables -t raw -I OUTPUT -p udp --sport 5353 -j CT --notrack
 iptables -t raw -I PREROUTING -p udp --dport 5353 -j CT --notrack
 iptables -I INPUT -p udp --dport 5353 -j ACCEPT
