8f22ba0db8a7b001de1d37ca44842083ba05b6d8
Oleksii Usatov <62352097+ousatov-ua@users.noreply.github.com>
2024-03-31 17:09:10 +0300

    Create install-bind9.sh

---
+++ opt/bind9/install-bind9.sh
@@ -0,0 +1,15 @@
+
+sudo apt install libjemalloc-dev
+sudo apt-get install -y pkg-config
+sudo apt-get install libuv1-dev
+sudo apt install openssl
+sudo apt install libssl-dev
+sudo apt-get install libcap-dev
+export CFLAGS="-O2"
+./configure --with-jemalloc=yes --with-tuning=large --disable-doh
+make
+make install
+
+
+vim ~/.bashrc <<- put this: export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib"
+sudo vim /etc/ld.so.conf.d/99local.conf  <<- put this: /usr/local/lib
