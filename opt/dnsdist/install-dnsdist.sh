d26fd9bd1c25bab4e91671e4d9da01e56b80cc05
Oleksii Usatov <62352097+ousatov-ua@users.noreply.github.com>
2024-04-01 01:40:57 +0300

    Update install-dnsdist.sh

--- opt/dnsdist/install-dnsdist.sh
+++ opt/dnsdist/install-dnsdist.sh
@@ -6,7 +6,8 @@ export CXXFLAGS="-Ofast -pipe -march=native -flto"
 
 # install rust (install-rust.sh)
 # install quich (install-quiche.sh)
+sudo ln /usr/local/lib/libdnsdist-quiche.so /usr/lib/libdnsdist-quiche.so
 
-./configure --enable-dns-over-tls --enable-dns-over-https --enable-dns-over-http3 --enable-dns-over-quic --with-systemd --prefix=/usr --with-quiche
+./configure --enable-dns-over-tls --enable-dns-over-https --enable-dns-over-http3 --enable-dns-over-quic --with-systemd --with-quiche
 make
 sudo make install
