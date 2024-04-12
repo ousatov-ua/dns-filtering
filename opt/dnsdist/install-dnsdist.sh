4765e261ff9996c03fa18f0ebbd9495866e85efd
Oleksii Usatov <62352097+ousatov-ua@users.noreply.github.com>
2024-04-13 00:29:50 +0300

    Update install-dnsdist.sh

--- opt/dnsdist/install-dnsdist.sh
+++ opt/dnsdist/install-dnsdist.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt install autoconf automake libedit-dev libsodium-dev libtool-bin pkg-config protobuf-compiler libnghttp2-dev libh2o-evloop-dev libluajit-5.1-dev libboost-all-dev libsystemd-dev libbpf-dev libclang-dev git
 sudo apt install cmake
-export CFLAGS="-Ofast -pipe -march=native"
-export CXXFLAGS="-Ofast -pipe -march=native"
-export CPPFLAGS="-Ofast -pipe -march=native"
+export CFLAGS="-O3 -pipe -march=znver3 -flto"
+export CXXFLAGS="-O3 -pipe -march=znver3 -flto"
+export CPPFLAGS="-O3 -pipe -march=znver3 -flto"
 
 # install rust (install-rust.sh)
 # install quich (install-quiche.sh)
