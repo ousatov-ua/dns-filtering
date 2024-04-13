ab2b76f32c79725401ea2efd472fbf0dc8f2876b
Oleksii Usatov <62352097+ousatov-ua@users.noreply.github.com>
2024-04-13 22:04:30 +0300

    Update install.sh

--- opt/powerdns/install.sh
+++ opt/powerdns/install.sh
@@ -4,7 +4,7 @@ export CFLAGS="-O3 -pipe -march=znver3 -flto"
 export CXXFLAGS="-O3 -pipe -march=znver3 -flto"
 export CPPFLAGS="-O3 -pipe -march=znver3 -flto"
 
-./configure --enable-dnstap --enable-lto --enable-systemd --with-libcap
+./configure --enable-dnstap --enable-lto --enable-systemd --with-libcap --with-libcurl=/usr/lib
 make
 make install
 
